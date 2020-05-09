15b507900182302c521680b4a16cfdbf2e9ba2e5
Luis Navarrete-Baduy <baduy97@users.noreply.github.com>
2020-05-08 21:24:20 -0500

    New

---
+++ week1/Projects/GreatCircle/src/greatcircle/GreatCircle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package greatcircle;
+
+/**
+ *
+ * @author DELL
+ */
+public class GreatCircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double x1 = Math.toRadians(Double.parseDouble(args[0]));
+        double y1 = Math.toRadians(Double.parseDouble(args[1]));
+        double x2 = Math.toRadians(Double.parseDouble(args[2]));
+        double y2 = Math.toRadians(Double.parseDouble(args[3]));
+        double r = 6371.0;
+        
+        double a = Math.pow(Math.sin((x2-x1)/2), 2);
+        double b = Math.cos(x1) * Math.cos(x2)* Math.pow(Math.sin((y2-y1)/2), 2);
+        
+        double c = 2*r* Math.asin(Math.sqrt(a+b));
+        
+        System.out.println(c + " kilometers");
+    }
+    
+}
