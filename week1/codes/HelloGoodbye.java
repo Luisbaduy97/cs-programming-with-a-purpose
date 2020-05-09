e40a7deabbc928567a8594e14ac1b018a5dd566b
Luis Navarrete-Baduy <baduy97@users.noreply.github.com>
2020-05-09 02:21:01 -0500

    New

---
+++ week1/codes/HelloGoodbye.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class HelloGoodbye {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String n1 = args[0], n2 = args[1];
+        
+        System.out.println("Hello " + n1 + " and " + n2);
+        System.out.println("Goodbye " + n2 + " and " + n1);
+    }
+    
+}
---
+++ week1/codes/RightTriangle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class RightTriangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = Integer.parseInt(args[0]), a2 = a*a;
+        int b = Integer.parseInt(args[1]), b2 = b*b;
+        int c = Integer.parseInt(args[2]), c2 = c*c;
+        
+        boolean co1 = c2 == a2+b2, co2 = a2 == c2+b2, co3 = b2 == c2+a2;
+        boolean f = co1||co2||co3, i = a>0 && b>0&& c>0;
+        boolean fin = f && i;
+        System.out.println(fin);
+    }
+    
+}
